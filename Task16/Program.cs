0b7e9226a021e71f40ccfcddddeb6dca9a14451b
Svetlana <start61@mail.ru>
2023-02-15 00:02:24 +0300

    Added Sem.2

---
+++ Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23.
+
+//14 -> нет 
+//46 -> нет 
+//161 -> да
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == number2*number2 || number1 * number1 == number2)
+{
+Console.Write("Да");
+}
+else
+{
+Console.Write("Нет");
+}
